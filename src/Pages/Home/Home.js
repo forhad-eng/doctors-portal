c144fbf3076c8dc746f3a8c7b41c98bddbb056a6
forhad-eng <forhadu.cse@gmail.com>
2022-05-11 12:47:05 +0600

    treatment banner section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Banner from './Banner'
+import ExtraInfo from './ExtraInfo'
 import Info from './Info'
 import Services from './Services'
 
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner />
             <Info />
             <Services />
+            <ExtraInfo />
         </div>
     )
 }
